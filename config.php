a9ae17a76450b277ff3447c07cd30adff781c97e
Artchibo7 <arthur.zulpukharov.simplon@gmail.com>
2024-04-17 10:21:49 +0200

    modif config

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'festival');
-define('DB_USER', 'festival');
-define('DB_PWD', 'festival');
-define('PREFIXE', 'mvf_');
+define('DB_NAME', 'sowesign');
+define('DB_USER', 'sowesign');
+define('DB_PWD', 'sowesign');
+define('PREFIXE', 'sws_');
 
 
 define('HOME_URL', '/');
 
 
 define('DB_INITIALIZED', TRUE);
+
